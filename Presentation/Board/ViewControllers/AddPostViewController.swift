edc607c88a3a7979cc61f6214268315f38e88579
hyunm1n-o <hmoh0703@naver.com>
2025-11-22 18:39:17 +0900

    Feat: 게시글 작성화면 추가

---
+++ Presentation/Board/ViewControllers/AddPostViewController.swift
@@ -0,0 +1,232 @@
+//
+//  AddPostViewController.swift
+//  LeafMAP
+//
+//  Created by 오현민 on 11/22/25.
+//
+
+import UIKit
+import PhotosUI
+
+class AddPostViewController: UIViewController {
+    // MARK: - Properties
+    let navigationBarManager = NavigationManager()
+    let storeCategory: String
+    private var keyboardHeight: CGFloat = 0
+    
+    // MARK: - View
+    private lazy var addPostView = AddPostView().then {
+        // 텍스트필드
+        $0.titleTextField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        $0.addressTextField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        
+        // 버튼
+        $0.postImageButton.addTarget(self, action: #selector(postImageButtonTapped), for: .touchUpInside)
+        $0.addButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
+    }
+    
+    // MARK: - Init
+    init(storeCategory: String) {
+        self.storeCategory = storeCategory
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(addPostView)
+        addPostView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGesture.cancelsTouchesInView = false // 버튼/텍스트뷰 터치 막지 않음
+        addPostView.scrollView.addGestureRecognizer(tapGesture)
+        
+        setupNavigationBar()
+        setupKeyboard()
+        setupTextViewDelegate()
+        updateAddButtonState()
+        updateAddButtonState()
+        
+        // 맛집 게시판만 주소 입력 가능
+        addPostView.setAddressFieldHidden(storeCategory != "맛집 게시판")
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    // MARK: - Navigation
+    private func setupNavigationBar() {
+        navigationBarManager.addBackButton(
+            to: navigationItem,
+            target: self,
+            action: #selector(prevVC),
+            tintColor: .black
+        )
+        
+        navigationBarManager.setTitle(
+            to: navigationItem,
+            title: storeCategory,
+            textColor: .gray900
+        )
+        
+        if let navBar = navigationController?.navigationBar {
+            navigationBarManager.addBottomLine(to: navBar)
+        }
+    }
+    
+    @objc private func prevVC() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
+    // MARK: - Actions
+    private func setupTextViewDelegate() {
+        addPostView.contentTextView.delegate = self
+    }
+    
+    // MARK: - 버튼 상태 업데이트
+    private func updateAddButtonState() {
+        let titleFilled = !(addPostView.titleTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ?? true)
+        let contentFilled = addPostView.contentTextView.textColor != .gray && !addPostView.contentTextView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+        
+        let addressFilled: Bool
+        if addPostView.addressTextField.isHidden {
+            addressFilled = true
+        } else {
+            addressFilled = !(addPostView.addressTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ?? true)
+        }
+        
+        let isFormValid = titleFilled && contentFilled && addressFilled
+        
+        addPostView.addButton.setButtonState(
+            isEnabled: isFormValid,
+            enabledColor: .green01,
+            disabledColor: .disable,
+            enabledTitleColor: .white,
+            disabledTitleColor: .white
+        )
+    }
+    
+    //MARK: Event
+    @objc
+    private func textFieldChanged() {
+        updateAddButtonState()
+    }
+    
+    @objc
+    private func addButtonTapped() {
+        print("게시글 등록")
+    }
+    
+    @objc
+    private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    // MARK: - Image Picker
+    @objc private func postImageButtonTapped() {
+        let alert = UIAlertController(title: "사진 선택", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "사진 선택", style: .default) { _ in self.openPhotoLibrary() })
+        alert.addAction(UIAlertAction(title: "촬영하기", style: .default) { _ in self.openCamera() })
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel))
+        present(alert, animated: true)
+    }
+    
+    private func openPhotoLibrary() {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        config.selectionLimit = 1
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    private func openCamera() {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else { return }
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    // MARK: - Keyboard
+    private func setupKeyboard() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGesture.cancelsTouchesInView = false // 버튼/텍스트뷰 터치 막지 않음
+        addPostView.scrollView.addGestureRecognizer(tapGesture)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let info = notification.userInfo,
+              let frame = info[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        keyboardHeight = frame.height
+        addPostView.scrollView.contentInset.bottom = keyboardHeight
+        addPostView.scrollView.verticalScrollIndicatorInsets.bottom = keyboardHeight
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        addPostView.scrollView.contentInset.bottom = 0
+        addPostView.scrollView.verticalScrollIndicatorInsets.bottom = 0
+    }
+}
+
+// MARK: - UITextViewDelegate
+extension AddPostViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == .gray {
+            textView.text = ""
+            textView.textColor = .gray900
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "내용을 입력하세요"
+            textView.textColor = .gray
+        }
+        updateAddButtonState()
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        updateAddButtonState()
+    }
+}
+
+// MARK: - PHPickerViewControllerDelegate
+extension AddPostViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        guard let result = results.first else { return }
+        result.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] object, _ in
+            if let image = object as? UIImage {
+                DispatchQueue.main.async {
+                    self?.addPostView.postImageView.image = image
+                    self?.addPostView.postImageButton.setImage(nil, for: .normal)
+                }
+            }
+        }
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate & UINavigationControllerDelegate
+extension AddPostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        picker.dismiss(animated: true)
+        if let image = info[.originalImage] as? UIImage {
+            addPostView.postImageView.image = image
+            addPostView.postImageButton.setImage(nil, for: .normal)
+        }
+    }
+}
---
+++ Presentation/Board/Views/AddPostView.swift
@@ -0,0 +1,143 @@
+//
+//  AddPostView.swift
+//  LeafMAP
+//
+//  Created by 오현민 on 11/22/25.
+//
+import UIKit
+
+class AddPostView: UIView {
+
+    // MARK: - Components
+    public lazy var scrollView = UIScrollView().then {
+        $0.showsVerticalScrollIndicator = true
+        $0.keyboardDismissMode = .interactive
+    }
+    
+    public lazy var contentView = UIView()
+    
+    public lazy var titleTextField = UITextField().then {
+        $0.placeholder = "게시글 제목을 입력하세요"
+        $0.font = UIFont(name: AppFontName.pMedium, size: 20)
+        $0.textColor = .gray900
+        $0.borderStyle = .none
+    }
+    
+    private lazy var seperateLine = UIView().then {
+        $0.backgroundColor = .disable
+        $0.snp.makeConstraints { $0.height.equalTo(1) }
+    }
+    
+    private lazy var imageLabel = AppLabel(text: "눌러서 이미지 추가", font:  UIFont(name: AppFontName.pRegular, size: 18)!, textColor: .gray).then {
+        $0.isUserInteractionEnabled = false
+    }
+    
+    public lazy var postImageButton = UIButton().then {
+        $0.backgroundColor = .gray1
+        $0.layer.cornerRadius = 12
+        $0.clipsToBounds = true
+    }
+
+    public lazy var postImageView = UIImageView().then {
+        $0.layer.cornerRadius = 12
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+    }
+    
+    public lazy var contentTextView = UITextView().then {
+        $0.text = "내용을 입력하세요"
+        $0.textColor = .gray
+        $0.font = UIFont(name: AppFontName.pRegular, size: 16)
+    }
+    
+    public lazy var addressTextField = UITextField().then {
+        $0.placeholder = "주소를 입력하세요"
+        $0.font = UIFont(name: AppFontName.pRegular, size: 16)
+        $0.textColor = .gray
+        $0.borderStyle = .none
+    }
+    
+    public lazy var addButton = AppButton(title: "게시글 등록하기", isEnabled: false)
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Views
+    private func setupViews() {
+        addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubviews([
+            titleTextField,
+            seperateLine,
+            postImageButton,
+            postImageView,
+            contentTextView,
+            addressTextField,
+            addButton,
+            imageLabel
+        ])
+    }
+    
+    private func setupConstraints() {
+        scrollView.snp.makeConstraints { $0.edges.equalToSuperview() }
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+
+        titleTextField.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(40)
+            $0.horizontalEdges.equalToSuperview().inset(24)
+            $0.height.equalTo(40)
+        }
+        
+        seperateLine.snp.makeConstraints {
+            $0.top.equalTo(titleTextField.snp.bottom).offset(4)
+            $0.horizontalEdges.equalToSuperview().inset(24)
+        }
+        
+        imageLabel.snp.makeConstraints {
+            $0.center.equalTo(postImageView)
+        }
+        
+        postImageButton.snp.makeConstraints {
+            $0.top.equalTo(seperateLine.snp.bottom).offset(24)
+            $0.horizontalEdges.equalToSuperview().inset(24)
+            $0.height.equalTo(200)
+        }
+        postImageView.snp.makeConstraints {
+            $0.edges.equalTo(postImageButton)
+        }
+        
+        contentTextView.snp.makeConstraints {
+            $0.top.equalTo(postImageView.snp.bottom).offset(24)
+            $0.horizontalEdges.equalToSuperview().inset(24)
+            $0.height.equalTo(200)
+        }
+        
+        addressTextField.snp.makeConstraints {
+            $0.top.equalTo(contentTextView.snp.bottom).offset(12)
+            $0.horizontalEdges.equalToSuperview().inset(24)
+            $0.height.equalTo(40)
+        }
+        
+        addButton.snp.makeConstraints {
+            $0.top.equalTo(addressTextField.snp.bottom).offset(24)
+            $0.bottom.horizontalEdges.equalToSuperview().inset(24)
+        }
+    }
+    
+    // MARK: - Helpers
+    func setAddressFieldHidden(_ hidden: Bool) {
+        addressTextField.isHidden = hidden
+    }
+}
